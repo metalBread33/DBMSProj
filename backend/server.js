f91d5b9c8a966d4e11762eb856c96fad24143bef
metalBread33 <kfoughty80@gmail.com>
2024-03-26 20:45:24 -0400

    API can access data through backend temp db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import pool from './database/data.js'
+import subkits from './database/temp.js';
 
 const Pool = pool
 const port = 5000;
@@ -8,6 +9,18 @@ const app = express()
 
 app.get('/', (req,res) => {
     res.send('API is running ...')
+})
+
+app.get('/api/individual', (req, res) => {
+    res.json(subkits)
+})
+
+app.get('/api/individual/:id', (req, res) => {
+    const item = subkits.find((p) => p.id == req.params.id);
+    if(!item)
+         res.status(404).json({error: "item not found"})
+    else
+        res.json(item)
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
