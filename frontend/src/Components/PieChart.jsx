58161d195b234b5f44a1cda339ebdd47205335b9
metalBread33 <kfoughty80@gmail.com>
2024-03-30 16:04:09 -0400

    Setup screen for single item

---
+++ frontend/src/Components/PieChart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PieChart = () => {
+  return (
+    <div>
+      PieChart goes here
+    </div>
+  )
+}
+
+export default PieChart
--- frontend/src/Screens/Individual.jsx
+++ frontend/src/Screens/Individual.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
+import PieChart from '../Components/PieChart'
 
 const Individual = () => {
+  
+  const {id : id} = useParams()
+  const [item, setItem] = useState([])
+  
+const getItem = async() => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/individual/${id}`)
+      const data = await response.json()
+
+      setItem(data[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+  }
+
+
+  useEffect(() => {
+    getItem()
+  }, [])
+  
   return (
     <div>
-      Screen to see individual nutrition info
+      <Row>
+        <Col md={6}>
+          <PieChart/>
+        </Col>
+        <Col md={6}>
+          <p>{item.name}</p>
+        </Col>
+      </Row>
     </div>
   )
 }
