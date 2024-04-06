50c5c32b18de1b2afbdcc60b53d8503d442bb80f
metalBread33 <kfoughty80@gmail.com>
2024-04-05 21:41:48 -0400

    Get piecharts up for individual items

--- frontend/src/Screens/Individual.jsx
+++ frontend/src/Screens/Individual.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
-import PieChart from '../Components/PieChart'
+//import PieChart from '../Components/PieChart'
+import {PieChart, Pie, ResponsiveContainer} from 'recharts'
 
 const Individual = () => {
   
@@ -19,6 +20,18 @@ const getItem = async() => {
     }
   }
 
+  const calsFromCarbs = item.carbs * 4;
+  const calsFromFat = item.fat  * 9;
+  const calsFromProtein = item.protein * 4;
+  const base = calsFromCarbs + calsFromCarbs + calsFromProtein;
+  
+  console.log(calsFromCarbs)
+  const pieData = [
+    {name: "Calories From Carbs", value: calsFromCarbs},
+    {name: "Calories From Fat", value: calsFromFat},
+    {name: "Calories From Protein", value: calsFromProtein},
+  ]
+
 
   useEffect(() => {
     getItem()
@@ -26,15 +39,22 @@ const getItem = async() => {
   
   return (
     <div>
+        <h1 style={{textAlign: 'center'}}>{item.name} Breakdown</h1>
+        <br/>
       <Row>
         <Col md={8}>
           {item.cals===0 ? 
-            <h2 style={{textAlign: 'center', bottom: 0, top: 15 }}>This item is calorie free</h2> : 
-          <PieChart/>
-         }
-        </Col>
+            <h2 style={{textAlign: 'center', margin: '15px auto' }}>This item is calorie free</h2> : (
+              
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart width={400} height={400}>
+                  <Pie data={pieData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" label/>
+                </PieChart>
+ 
+              </ResponsiveContainer>
+        )
+      }</Col>
         <Col md={4}>
-          <h2>{item.name}</h2>
           <h3>{item.cals} Calories</h3>
           <h3>{item.carbs}g of Carbs</h3>
           <h3>{item.fat}g of Fat</h3>
