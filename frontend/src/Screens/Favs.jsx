20a5ecc88e82c2bfdeeaaf268e69d95e0815c7d6
metalBread33 <kfoughty80@gmail.com>
2024-04-18 11:45:47 -0400

    Set up table for favorites

--- frontend/src/Screens/Favs.jsx
+++ frontend/src/Screens/Favs.jsx
@@ -10,22 +10,61 @@ const Favs = () => {
   const [favs, setFavs] = useState([])
 
   const getFavs = async () => {
-    const email = auth.user.email
-    const body = {email}
-    const response = await fetch(`http://localhost:5000/get/fav/${email}`)
-    const data = await response.json()
-    setFavs(data)
+    try {
+      const email = auth.user.email
+      const response = await fetch(`http://localhost:5000/get/fav/${email}`)
+      const data = await response.json()
+      setFavs(data)
+
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   useEffect(() => {
     if(!auth.user)
       nav('/')
     getFavs()
+    console.log(favs)
   }, [])
   return (
-    <div>
-      Users can see favorite sub orders
-    </div>
+    <>
+      <Col>
+        <Row>
+          <Table striped hover responsive className='table-lg'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {favs.map((fav) => 
+                <tr key={fav.subid}>
+                  <td>{fav.subname}</td>
+                  <td>
+                    <Button style={{color: 'black'}}
+                      variant='link'
+                      onClick={() => console.log("edit")}>
+                        <FaEdit/>
+                    </Button>
+                  </td>
+                  <td>
+                    <Button style={{color: 'black'}}
+                      variant='link'
+                      onClick={() => console.log("delete")}>
+                        <FaTrash/>
+                    </Button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </Row>
+      </Col>
+    </>
   )
 }
 
